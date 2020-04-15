c7417a15381682441b19f6a6775ec2ae82753deb
mohammad ahamd <ma3571318z@gmail.com>
2020-04-15 13:09:25 +0300

    1st commit for home page

---
+++ js/init-swiper.js
@@ -0,0 +1,16 @@
+// Install Plugin To Swiper
+Swiper.use(myPlugin);
+
+// Init Swiper
+var swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Enable debugger
+  debugger: true,
+});
